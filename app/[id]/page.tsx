3ef1c764c4ea36270493dfce2796e7cc5b2e776f
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-03-24 23:30:04 +0100

    complete fix responsive. Change fonts. Add header

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Down from "../assets/downVector.png";
 import Up from "../assets/upVector.png";
-import Footer from "../components/Footer";
+import Header from "../components/Header";
 import SelectOption from "./SelectOption";
 import "./preparation.scss";
 import {
@@ -26,6 +26,7 @@ const PreparationPage = ({ params: { id } }: Props) => {
 
 	return (
 		<main className="container-preparation-wrapper">
+			<Header />
 			<section className="container-preparation-content">
 				<h2 className="title-type-question">{id} Вопросы</h2>
 				<section className="section-select-opions">
@@ -60,6 +61,28 @@ const PreparationPage = ({ params: { id } }: Props) => {
 						Это две стратегии веб-разработки, которые подразумевают поэтапное
 						создание веб-страниц с учетом возможностей и ограничений различных
 						браузеров и устройств.
+						<br />
+						<br />
+						Это две стратегии веб-разработки, которые подразумевают поэтапное
+						создание веб-страниц с учетом возможностей и ограничений различных
+						браузеров и устройств.
+						<br />
+						<br />
+						Это две стратегии веб-разработки, которые подразумевают поэтапное
+						создание веб-страниц с учетом возможностей и ограничений различных
+						браузеров и устройств.
+						<br />
+						<br />
+						Это две стратегии веб-разработки, которые подразумевают поэтапное
+						создание веб-страниц с учетом возможностей и ограничений различных
+						браузеров и устройств.
+						<br />
+						<br />
+						Это две стратегии веб-разработки, которые подразумевают поэтапное
+						создание веб-страниц с учетом возможностей и ограничений различных
+						браузеров и устройств.
+						<br />
+						<br />
 					</p>
 					<button
 						className={`button-next-question ${isActive ? "" : "active"}`}
@@ -68,8 +91,6 @@ const PreparationPage = ({ params: { id } }: Props) => {
 					</button>
 				</section>
 			</section>
-
-			<Footer />
 		</main>
 	);
 };
