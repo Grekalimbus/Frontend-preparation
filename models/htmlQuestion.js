b3b7aa34910cd0d06ea40d6650cad0740d7cb645
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-03-25 19:32:09 +0100

    connect to DB and create first requests

---
+++ models/htmlQuestion.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const htmlSchema = new Schema({
+	question: String,
+	answer: String,
+});
+
+const HTML =
+	mongoose.models.HTML || mongoose.model("HTML Question", htmlSchema);
+
+export default HTML;
