3df9ddf216dfe7d6d4307ee1fb69391aa7e09c1b
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-04-02 01:55:45 +0200

    change readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
 	title: "Frontend вопросы с собеседований",
 	description:
 		"Шпаргалка для подготовки к собеседованиям. Вопросы с собеседований на Frontend разработчика. Вопросы HTML / CSS / JS / TS / React / Redux и общие вопросы",
-	manifest: "./assets/iconLink.png",
 };
 
 export default function RootLayout({
