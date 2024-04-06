5ad991d0e2030c01037162321368616da9dc1ff6
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-04-06 20:12:11 +0200

    change htmlQuestion > [id] > route. Route is work correct. Need change rest dinamyc routes

--- app/api/htmlQuestion/[id]/route.js
+++ app/api/htmlQuestion/[id]/route.js
@@ -2,14 +2,7 @@ import connectMongoDB from "@/libs/mongodb";
 import HtmlQuestionModel from "@/models/htmlQuestion";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params }) {
-	const { id } = params;
-	await connectMongoDB();
-	const html = HtmlQuestionModel.findOne({ _id: id });
-	return NextResponse.json({ html }, { status: 200 });
-}
-
-export async function PATCH(request, { params }) {
+export async function PUT(request, { params }) {
 	const { id } = params;
 	const {
 		newQuestion: question,
@@ -18,5 +11,12 @@ export async function PATCH(request, { params }) {
 	} = await request.json();
 	await connectMongoDB();
 	await HtmlQuestionModel.findByIdAndUpdate(id, { question, answer, category });
-	return NextResponse.json({ message: "Questiom updated" }, { status: 200 });
+	return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+	const { id } = params;
+	await connectMongoDB();
+	const html = await HtmlQuestionModel.findOne({ _id: id });
+	return NextResponse.json({ html }, { status: 200 });
 }
