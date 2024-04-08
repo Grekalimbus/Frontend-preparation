38140ef803d441d830aa3d77e6e767f7689616ef
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-04-08 16:06:17 +0200

    add CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,DELETE,PATCH,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
+
 	env: {
 		SECRET_KEY_ADMIN: process.env.SECRET_KEY_ADMIN,
 	},
+
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
