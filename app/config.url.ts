de039a4780514ed39ed0473361987290878106de
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-05-06 18:52:04 +0200

    changed api

--- app/config.url.ts
+++ app/config.url.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://frontend-preparation-gamma.vercel.app/api/";
+export const BASE_URL = "http://localhost:3000/api/";
 // http://localhost:3000/api/
 // https://frontend-preparation-gamma.vercel.app/api/
